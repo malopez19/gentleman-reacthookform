639c3c2a5b2c129c72cab9d3e9bb230ea3c0031d
malopez19 <lopezmateojurado@gmail.com>
2025-02-23 19:27:13 -0500

    update form validation messages to Spanish for better localization

--- src/models/form.model.ts
+++ src/models/form.model.ts
@@ -3,11 +3,11 @@ import { z } from "zod";
 export const schema = z
   .object({
     name: z.string().min(1, "Nombre es requerido"),
-    email: z.string().email("Correo invalido").min(1, "Email is required"),
-    password: z.string().min(6, "Password must be at least 8 characters"),
-    confirmPassword: z.string().min(6, "Password must be at least 6 characters"),
+    email: z.string().email("Correo invalido").min(1, "Correo es requerido"),
+    password: z.string().min(6, "Contraseña debe tener al menos 6 caracteres"),
+    confirmPassword: z.string().min(6, "Contraseña debe tener al menos 6 caracteres"),
   }).refine(data => data.password === data.confirmPassword, {
-    message: "Passwords do not match",
+    message: "Las contraseñas no coinciden",
     path: ["confirmPassword"],
   });
 
